58d20e29989afc85fa37c8829c3a452bcdfb068d
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-07-28 21:41:05 +0500

    Goals controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -16,10 +16,11 @@ const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error("Please add a text field")
-    })
+    }
 
-res.status(200).json({
-    message: "Set goals"
+    res.status(200).json({
+        message: "Set goals"
+    })
 })
 
 // @desc    Update goal
